fa2ca723ab542b395d9043cdb5eaf97f705f5d7c
Sergey Korchmin <sergey.korchmin@yandex.ru>
2024-04-20 19:23:37 +0300

    added title in base.html

--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,10 @@ from catalog.models import Product
 
 
 def home(request):
-    context = {'object_list': Product.objects.all()}
+    context = {'object_list': Product.objects.all(),
+               'title': 'Всячина',
+               'description_title': 'Всячина - это онлайн магазин абсолютно разных товаров'
+                                    '(от хлеба до крыла от самолёта) по доступной цене и отличного качества'}
     return render(request, 'catalog/home.html', context)
 
 
@@ -15,9 +18,12 @@ def contacts(request):
         message = request.POST.get('message')
 
         print(f"Имя:{name}\nТелефон: {phone}\nСообщение: {message}")
-    return render(request, 'catalog/contacts.html')
+
+    context = {'title': 'Контакты', 'description_title': 'Свяжитесь с нами и мы вам обязательно поможем!'}
+    return render(request, 'catalog/contacts.html', context)
 
 
 def product(request, pk):
-    context = {'object': Product.objects.get(pk=pk)}
+    context = {'object': Product.objects.get(pk=pk),
+               'title': 'Товары', 'description_title': 'Самые лучшие товары отличного качества'}
     return render(request, 'catalog/product.html', context)
