afc92728023e806941fb80eaa5af3e54409091be
Sergey Korchmin <sergey.korchmin@yandex.ru>
2024-04-20 13:36:33 +0300

    added static

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,7 +9,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Product)
-class CategoryAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
