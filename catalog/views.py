a378358dd6602237f6f01fbdd2e7552cdf46fca0
Sergey Korchmin <sergey.korchmin@yandex.ru>
2024-04-20 18:38:22 +0300

    added product.html, base.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {'object_list': Product.objects.all()}
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -13,3 +16,8 @@ def contacts(request):
 
         print(f"Имя:{name}\nТелефон: {phone}\nСообщение: {message}")
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    context = {'object': Product.objects.get(pk=pk)}
+    return render(request, 'catalog/product.html', context)
