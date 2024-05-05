f8a4fd485088e04da783be5da3fa680e71bfaea7
Sergey Korchmin <sergey.korchmin@yandex.ru>
2024-05-05 14:48:34 +0300

    reformat FBV to CBV (some change)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, ProductListView, ProductDetailView
+from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product')
+    path('', ProductListView.as_view(), name='product_list'),
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'catalog/home.html'
     extra_context = {'title': 'Всячина', 'description_title': 'Всячина - это онлайн магазин абсолютно разных товаров '
                                                               '(от хлеба до крыла от самолёта) по доступной цене и '
                                                               'отличного качества'}
@@ -14,17 +13,17 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'catalog/product.html'
     extra_context = {'title': 'Товары', 'description_title': 'Самые лучшие товары отличного качества'}
 
 
-def contacts(request):
-    if request.method == 'POST':
+class ContactsTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
+    extra_context = {'title': 'Контакты', 'description_title': 'Свяжитесь с нами и мы вам обязательно поможем!'}
+
+    def post(self, request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
 
         print(f"Имя:{name}\nТелефон: {phone}\nСообщение: {message}")
-
-    context = {'title': 'Контакты', 'description_title': 'Свяжитесь с нами и мы вам обязательно поможем!'}
-    return render(request, 'catalog/contacts.html', context)
+        return HttpResponseRedirect('/')
