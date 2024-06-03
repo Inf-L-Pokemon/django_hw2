620540723a818286b64516b797afba280df1358f
Sergey Korchmin <sergey.korchmin@yandex.ru>
2024-06-04 00:22:34 +0300

    настроена регистрация, вход и выход пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserCreateForm
+from users.models import User
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreateForm
+    success_url = reverse_lazy('users:login')
